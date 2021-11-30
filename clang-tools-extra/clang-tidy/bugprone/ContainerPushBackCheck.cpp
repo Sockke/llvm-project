5ff19b91e68a0f2b90bed44a80c044acb5c0076d
Sockke <liuke.gehry@bytedance.com>
2021-11-30 14:57:58 +0800

    support list container

--- clang-tools-extra/clang-tidy/bugprone/ContainerPushBackCheck.cpp
+++ clang-tools-extra/clang-tidy/bugprone/ContainerPushBackCheck.cpp
@@ -19,51 +19,51 @@ namespace tidy {
 namespace bugprone {
 
 void ContainerPushBackCheck::registerMatchers(MatchFinder *Finder) {
-  const auto VectorDecl = cxxRecordDecl(hasName("::std::vector"));
+  const auto ContainerDecl = cxxRecordDecl(hasAnyName("::std::vector", "::std::list"));
   const auto AppendMethodDecl =
       cxxMethodDecl(hasAnyName("push_back", "emplace_back"));
-  const auto VectorConstructorCall = cxxConstructExpr(
-      hasType(VectorDecl),
-      hasDeclaration(cxxConstructorDecl(hasName("vector"))), argumentCountIs(1),
+  const auto ContainerConstructorCall = cxxConstructExpr(
+      hasType(ContainerDecl),
+      hasDeclaration(cxxConstructorDecl(hasAnyName("vector", "list"))), argumentCountIs(1),
       hasArgument(0, hasType(qualType(isInteger()))));
   const auto TargetVarDecl =
-      varDecl(hasInitializer(VectorConstructorCall)).bind("vector_var_decl");
+      varDecl(hasInitializer(ContainerConstructorCall)).bind("container_var_decl");
   const auto TargetVarDefStmt =
-      declStmt(hasSingleDecl(TargetVarDecl)).bind("vector_var_decl_stmt");
+      declStmt(hasSingleDecl(TargetVarDecl)).bind("container_var_decl_stmt");
   const auto AppendCallExpr =
       cxxMemberCallExpr(
-          callee(AppendMethodDecl), on(hasType(VectorDecl)),
+          callee(AppendMethodDecl), on(hasType(ContainerDecl)),
           onImplicitObjectArgument(declRefExpr(to(TargetVarDecl))))
-          .bind("vector_append_call");
+          .bind("container_append_call");
   const auto AppendCall = expr(AppendCallExpr);
-  const auto CompoundStmtWithVector =
-      compoundStmt(has(TargetVarDefStmt)).bind("compound_stmt_with_vector");
-  const auto FunctionDeclWithVector =
+  const auto CompoundStmtWithContainer =
+      compoundStmt(has(TargetVarDefStmt)).bind("compound_stmt_with_container");
+  const auto FunctionDeclWithContainer =
       functionDecl(
-          hasBody(allOf(compoundStmt(has(AppendCall)), CompoundStmtWithVector)))
-          .bind("function_decl_with_vector");
-  Finder->addMatcher(FunctionDeclWithVector, this);
+          hasBody(allOf(compoundStmt(has(AppendCall)), CompoundStmtWithContainer)))
+          .bind("function_decl_with_container");
+  Finder->addMatcher(FunctionDeclWithContainer, this);
 }
 
 void ContainerPushBackCheck::check(const MatchFinder::MatchResult &Result) {
-  const auto *FunctionDeclWithVector =
-      Result.Nodes.getNodeAs<FunctionDecl>("function_decl_with_vector");
-  if (!FunctionDeclWithVector)
+  const auto *FunctionDeclWithContainer =
+      Result.Nodes.getNodeAs<FunctionDecl>("function_decl_with_container");
+  if (!FunctionDeclWithContainer)
     return;
   const auto *TargetVarDecl =
-      Result.Nodes.getNodeAs<VarDecl>("vector_var_decl");
+      Result.Nodes.getNodeAs<VarDecl>("container_var_decl");
   const auto *TargetVarDefStmt =
-      Result.Nodes.getNodeAs<DeclStmt>("vector_var_decl_stmt");
+      Result.Nodes.getNodeAs<DeclStmt>("container_var_decl_stmt");
   const auto *AppendCallExpr =
-      Result.Nodes.getNodeAs<CXXMemberCallExpr>("vector_append_call");
-  const auto *CompoundStmtWithVector =
-      Result.Nodes.getNodeAs<CompoundStmt>("compound_stmt_with_vector");
+      Result.Nodes.getNodeAs<CXXMemberCallExpr>("container_append_call");
+  const auto *CompoundStmtWithContainer =
+      Result.Nodes.getNodeAs<CompoundStmt>("compound_stmt_with_container");
   if (!TargetVarDecl || !TargetVarDefStmt || !AppendCallExpr)
     return;
   auto *Context = Result.Context;
   llvm::SmallPtrSet<const DeclRefExpr *, 16> AllVarRefs =
       utils::decl_ref_expr::allDeclRefExprs(*TargetVarDecl,
-                                            *CompoundStmtWithVector, *Context);
+                                            *CompoundStmtWithContainer, *Context);
   SourceManager &SM = Result.Context->getSourceManager();
   for (const auto *Ref : AllVarRefs) {
     if (Ref != AppendCallExpr->getImplicitObjectArgument())
